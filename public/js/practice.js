7e10ce5b62409423566ab75921609f3dbe916963
Shanmukha P <79988783+shanmukhapolu@users.noreply.github.com>
2026-09-17 21:58:59 -0400

    Fix duplicate date formatter and refresh module versions

--- public/js/practice.js
+++ public/js/practice.js
@@ -1,4 +1,4 @@
-import { db } from "./firebase.js?v=20260918-02";
+import { db } from "./firebase.js?v=20260918-03";
 import { doc, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
 
 let currentUser=null;
