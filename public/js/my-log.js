5fac4a9b9b670ae79e7fd9f80b23df5664c9261f
Shanmukha P <79988783+shanmukhapolu@users.noreply.github.com>
2026-09-17 21:52:27 -0400

    Bust cached local module dependencies

--- public/js/my-log.js
+++ public/js/my-log.js
@@ -1,6 +1,6 @@
-import { db } from "./firebase.js";
+import { db } from "./firebase.js?v=20260918-02";
 import { doc, getDoc, onSnapshot, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
-import { showToast } from "./ui.js";
+import { showToast } from "./ui.js?v=20260918-02";
 
 let currentUser = null;
 let practice = null;
